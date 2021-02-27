84fbb708346337943856fa66d4a5a5a9118e0830
Tejraj-miniyar <tejrajminiyar@gmail.com>
2021-02-27 16:00:06 +0530

    Initial learn

---
+++ practice.py
@@ -0,0 +1,13 @@
+import mysql.connector
+#step 1: create connection with MYSQL
+con=mysql.connector.connect(host="127.0.0.1",user="root",password="")
+
+#step 2: create cursor object that can execute sql query
+cursor=con.cursor();
+#step 3: execute sql query with the help of cursor object
+cursor.execute("show databases")
+
+#iterate elements in cursor
+print(cursor)
+for records in cursor:
+    print(records)
